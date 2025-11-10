964578cc06914e6a8c5ae84c847e590d55e6d1c4
Chris Erin <chris.erin@gmail.com>
2025-11-10 15:28:42 -0500

    fix(midi): Open ports with increasing ids

--- smplrmidi/smplrmidi.go
+++ smplrmidi/smplrmidi.go
@@ -9,12 +9,14 @@ import (
 
 func Start(midiChannel chan midi.Message) (func(), error) {
 
+	largestID := FindLargestSmplrMidiID()
+
 	driver, err := rtmididrv.New()
 	if err != nil {
 		fmt.Println("Can't open MIDI driver:", err)
 
 	}
-	in, err := driver.OpenVirtualIn("smplr-midi-in")
+	in, err := driver.OpenVirtualIn(fmt.Sprintf("smplr-midi-in-%d", largestID+1))
 	if err != nil {
 		fmt.Println("Can't open virtual MIDI input port:", err)
 	}
@@ -35,4 +37,20 @@ func Start(midiChannel chan midi.Message) (func(), error) {
 		return nil, fmt.Errorf("failed to listen to MIDI input: %w", err)
 	}
 	return stop, nil
+}
+
+func FindLargestSmplrMidiID() int {
+	outports := midi.GetOutPorts()
+	var largestSmplrID int
+	for _, outport := range outports {
+		name := outport.String()
+		var smplrID int
+		n, err := fmt.Sscanf(name, "smplr-midi-in-%d", &smplrID)
+		if err == nil && n == 1 {
+			if smplrID > largestSmplrID {
+				largestSmplrID = smplrID
+			}
+		}
+	}
+	return largestSmplrID
 }
